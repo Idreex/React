ab556c0608566abecce4598bfa50111074376315
Idreex <idreesademola07@gmail.com>
2023-12-03 17:13:07 +0100

    updating

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Buttonclick from "./components/Click"
+import TeacherList from "./components/propsConcept/Teacher"
+
+function App(){
+  return(
+    <div>
+
+      <h1>Welcome Home</h1>
+      <Buttonclick />
+      <TeacherList />
+
+    </div>
+    
+  )
+}
+
+
+export default App
